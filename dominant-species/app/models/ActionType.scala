7a8c676b6efd835f1db36b7b7153dcfc342c7a7f
Robert Strack <robert.strack@gmail.com>
2013-04-20 15:15:24 -0400

    added remaining action types

--- dominant-species/app/models/ActionType.scala
+++ dominant-species/app/models/ActionType.scala
@@ -12,7 +12,67 @@ object InitiativeAction extends ActionType {
 
 }
 
+object AdaptationAction extends ActionType {
+
+  override val actionNumber = 3
+
+}
+
+object RegressionAction extends ActionType {
+
+  override val actionNumber = 2
+
+}
+
 object AbundanceAction extends ActionType {
+
+  override val actionNumber = 2
+
+}
+
+object WastelandAction extends ActionType {
+
+  override val actionNumber = 1
+
+}
+
+object DepletionAction extends ActionType {
+
+  override val actionNumber = 1
+
+}
+
+object GlaciationAction extends ActionType {
+
+  override val actionNumber = 4
+
+}
+
+object SpeciationAction extends ActionType {
+
+  override val actionNumber = 6
+
+}
+
+object WanderlustAction extends ActionType {
+
+  override val actionNumber = 3
+
+}
+
+object MigrationAction extends ActionType {
+
+  override val actionNumber = 6
+
+}
+
+object CompetitionAction extends ActionType {
+
+  override val actionNumber = 7
+
+}
+
+object DominationAction extends ActionType {
 
   override val actionNumber = 3
 
