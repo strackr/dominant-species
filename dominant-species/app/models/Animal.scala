543599c4ff15b8b8ff250e0ca7a3f2092fe83c71
Robert Strack <robert.strack@gmail.com>
2013-04-22 15:08:28 -0400

    simplified class animal

--- dominant-species/app/models/Animal.scala
+++ dominant-species/app/models/Animal.scala
@@ -5,22 +5,16 @@ class Animal(val animalType: AnimalType, val elements: Map[ElementType, Int],
 
   val elementsCapacity = 6
 
+  def elementsNumber = elements.values.sum
+
+  def canAdapt = elementsNumber < elementsCapacity
+
   def getDominanceValue(fieldElements: Map[ElementType, Int]) = {
     elements.foldLeft(0) {
       case (acc, (elem, count)) => {
         acc + count * fieldElements.getOrElse(elem, 0)
       }
     }
-  }
-
-  def elementsNumber = {
-    elements.foldLeft(0) {
-      case (acc, (key, value)) => acc + value
-    }
-  }
-
-  def canAdapt = {
-    elementsNumber < elementsCapacity
   }
 
 }
